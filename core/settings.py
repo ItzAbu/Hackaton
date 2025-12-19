ec2a342a2f25eb01545f1d94186b659f868c02e3
ItzAbu <107542973+ItzAbu@users.noreply.github.com>
2025-12-19 02:09:59 +0100

    db change

--- core/settings.py
+++ core/settings.py
@@ -94,7 +94,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
     )
 }
 
