663b2d6ba47247db640f69aeb6416bdea0b67286
ItzAbu <107542973+ItzAbu@users.noreply.github.com>
2025-12-19 06:06:30 +0100

    Demo Game Implemented

--- game/urls.py
+++ game/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-
+    path("demo/", views.demo, name="game_demo"),
 ]
--- game/views.py
+++ game/views.py
@@ -1,4 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+@login_required
+def demo(request):
+    """UrbanKnowledge demo game (Vite build)"""
+    return render(request, "game/demo.html")
 
