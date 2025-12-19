294224e3043cbf86c52be92f859c2cabcce6f567
ItzAbu <107542973+ItzAbu@users.noreply.github.com>
2025-12-19 06:28:14 +0100

    fix 2

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,12 +6,12 @@ from . import views
 urlpatterns = [
     path("login/", views.CustomLoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
-    path("register/", views.register, name="register"),
+    path("register/", views.CustomLoginView.register, name="register"),
 
     # compat / redirect
-    path("after-login/", views.after_login, name="after_login"),
+    path("after-login/", views.CustomLoginView.after_login, name="after_login"),
 
     # landing
-    path("", views.home, name="lobby"),
-    path("home/", views.home, name="home"),
+    path("", views.CustomLoginView.home, name="lobby"),
+    path("home/", views.CustomLoginView.home, name="home"),
 ]
