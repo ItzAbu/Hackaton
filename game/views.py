23d5ed395d84eaf4ebbac779cd9b984a6aeb9f01
ItzAbu <107542973+ItzAbu@users.noreply.github.com>
2025-12-19 06:50:03 +0100

    gameplay no login

--- game/views.py
+++ game/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-@login_required
 def demo(request):
     """UrbanKnowledge demo game (Vite build)"""
     return render(request, "game/demo.html")
